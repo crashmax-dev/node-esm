f3399f17ee9cd65607888ee203574d06b58b2015
Vitalij Ryndin <sys@crashmax.ru>
2021-11-04 17:42:17 +0800

    chore: removed global declarations

--- declaration.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    DOTENV_EXAMPLE: string
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-console.log('✅ dotenv:', process.env.DOTENV_EXAMPLE)
+console.log('✅ dotenv:', process.env['DOTENV_EXAMPLE'])
