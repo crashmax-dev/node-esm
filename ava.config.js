6ab3405cf411ff5cfdba720f9ea10be0bf054826
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-06-29 13:57:51 +0800

    chore: update dependencies

--- ava.config.js
+++ ava.config.js
@@ -1,7 +1,13 @@
 export default {
-  files: ['./packages/**/*.test.ts', './apps/**/*.test.ts'],
+  files: [
+    './packages/**/*.test.ts',
+    './apps/**/*.test.ts'
+  ],
   extensions: {
     ts: 'module'
   },
-  nodeArguments: ['--loader=tsx', '--no-warnings']
+  nodeArguments: [
+    '--loader=tsx',
+    '--no-warnings'
+  ]
 }
